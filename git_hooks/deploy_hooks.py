efe7e63125a306745866c55e6083c35df5425c25
Shahzeb Ihsan <shahzeb@gmail.com>
2015-07-10 01:01:39 +0800

    Added Node.js post-commit hook

--- git_hooks/deploy_hooks.py
+++ git_hooks/deploy_hooks.py
@@ -3,3 +3,7 @@ import os, sys, shutil
 
 if(os.path.exists('python-post-commit') and os.path.exists(os.path.join('..', '..', 'python'))):
     shutil.copyfile('python-post-commit', os.path.join('..', '..', 'python', '.git', 'hooks', 'post-commit'))
+
+if(os.path.exists('nodejs-post-commit') and os.path.exists(os.path.join('..', '..', 'nodejs'))):
+    shutil.copyfile('nodejs-post-commit', os.path.join('..', '..', 'nodejs', '.git', 'hooks', 
+'post-commit'))
